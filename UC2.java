9bf16ba9736632bb7a6cd27df05f5fba94fd6e41
Suresh955-pro <gr9301@srmist.edu.in.com>
2026-02-23 22:13:32 +0530

    Updated UC2 with code

---
+++ UC2.java
@@ -0,0 +1,14 @@
+
+class UC2
+{
+    public static void main(String Args[])
+        {
+            System.out.println("   ***       ***      ******      *****   ");
+            System.out.println("  **  **    **  **    **   **    **   **  ");
+            System.out.println(" **    **  **    **   **   **    **       ");
+            System.out.println(" **    **  **    **   ******      *****   ");
+            System.out.println(" **    **  **    **   **              **  ");
+            System.out.println("  **  **    **  **    **         **   **  ");
+            System.out.println("   ***       ***      **          *****   ");
+        }
+}
