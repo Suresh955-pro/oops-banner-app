9b0f5562d85bdf24cbddc03a72fb04240203401e
Suresh955-pro <gr9301@srmist.edu.in.com>
2026-03-06 09:56:05 +0530

    use case 3

--- UC3.java
+++ UC3.java
@@ -2,7 +2,7 @@
 public class UC3 {
 
     public static void main(String[] args) {
-
+        /*Use case 3 */
         System.out.println(String.join(" ", "   ***    ", "   ***    ", "  ****** ", "   *****  "));
         System.out.println(String.join(" ", "  **  **  ", "  **  **  ", "  **   ** ", " **   ** "));
         System.out.println(String.join(" ", " **    ** ", " **    ** ", "  **   ** ", " **      "));
